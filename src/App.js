cd9e6832418a39ad8e11f184a5270bde487c3e75
NTD-Funix <ntd.funix@gmail.com>
2022-03-19 23:45:20 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, { Component } from 'react';
 import './App.css';
+import {DISHES} from './shared/dishes';
 import Menu from './components/MenuComponent';
 
 
-function App() {
-  return (
-    <div className="App">
-        <Menu/>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+        }
+    }
+    
+    render() {
+        return (
+          <div className="App">
+              <Menu dishes={this.state.dishes}/>
+          </div>
+        );
+    }
 }
 
 export default App;
